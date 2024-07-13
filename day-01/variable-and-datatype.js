4704d81f5870ed52205a58062d66c75024fba69f
ArmanRuhit <armanruhit09@gmail.com>
2024-07-13 17:14:13 +0600

    day-01

---
+++ day-01/variable-and-datatype.js
@@ -0,0 +1,13 @@
+var number = 10;
+console.log(number);
+console.log(typeof number);
+
+let string = "Hello";
+console.log(string);
+console.log(typeof string);
+
+const bool = true;
+console.log(bool)
+console.log(typeof bool);
+
+bool = false; //will generate an error
