7ed72e7c446da84d6c1ca1b1e9d38ae4332de251
ArmanRuhit <armanruhit09@gmail.com>
2024-07-29 02:22:43 +0600

    Day 02

---
+++ day-02/operators.js
@@ -0,0 +1,63 @@
+// Task 1: Add two numbers
+let num1 = 1;
+let num2 = 2;
+
+console.log("Task 1")
+console.log(num1+num2);
+
+// Task 2: Subtract two number
+console.log("Task 2")
+console.log(num1-num2);
+
+// Task 3: Multiply two number
+console.log("Task 3")
+console.log(num1*num2);
+
+// Task 4: Divide two number
+console.log("Task 4")
+console.log(num1/num2);
+
+// Task 5: Find the remainder when one number is divided by another number
+console.log("Task 5")
+console.log(num1%num2);
+
+// Task 6: Use the += operator
+num1+=num2;
+console.log("Task 6")
+console.log(num1);
+
+// Task 7: Use the -= operator
+num1-=num2;
+console.log("Task 7")
+console.log(num1)
+
+// Task 8: Compare two numbers using > and <
+console.log("Task 8")
+console.log(num1 < num2)
+console.log(num1 > num2)
+
+// Task 9: Compare two numbers using >= and <=
+console.log("Task 9");
+console.log(num1 <= num2);
+console.log(num1 >= num2);
+
+// Task 10: Compare two numbers using == and ===
+console.log("Task 10");
+console.log(num1 == num2);
+console.log(num1 === num2);
+
+// Task 11: Use &&
+console.log("Task 11");
+console.log(num1 && num2);
+// Task 12: Use ||
+console.log("Task 12");
+console.log(num1 || num2);
+// Task 13: Use !
+console.log("Task 13");
+console.log(!num1);
+console.log(!num2);
+
+// Task 14: Use ternary operator
+console.log("Task 14")
+console.log(num1 < num2 ? true : false)
+
